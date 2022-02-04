0086c90396b94342629380aa4de5cf52d7ade51a
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2022-02-04 14:48:15 +0100

    using rocket for backend

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+#![feature(decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+mod back_office;
+
+use rocket_contrib::{serve::StaticFiles, templates::Template};
+use serde::Serialize;
+
+#[derive(Clone, Serialize)]
+pub struct Texts {
+    homepage_1: String,
+    homepage_2: String,
+}
+
+#[get("/")]
+fn homepage() -> Template {
+    Template::render(
+        "homepage",
+        Texts {
+            homepage_1: String::from("hello"),
+            homepage_2: String::from("hello2"),
+        },
+    )
+}
+
+#[get("/about")]
+fn about() -> Template {
+    Template::render("about", "")
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![homepage, about])
+        .mount("/back-office", routes![back_office::back_office])
+        .mount("/static", StaticFiles::from("static"))
+        .attach(Template::fairing())
+        .launch();
+}
