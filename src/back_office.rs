fbb11a461cff78974744f5312e3535a112c78d02
Morgan Van Amerongen <vanamerongen.morgan@gmail.com>
2022-02-10 12:10:07 +0100

    texts in file

--- src/back_office.rs
+++ src/back_office.rs
@@ -1,18 +1,7 @@
-use crate::Texts;
+use crate::load_texts;
 use rocket_contrib::templates::Template;
-use std::collections::HashMap;
 
 #[get("/")]
 pub fn back_office() -> Template {
-    let context: HashMap<&str, Texts> = [(
-        "texts",
-        Texts {
-            homepage_1: String::from("hello"),
-            homepage_2: String::from("hello2"),
-        },
-    )]
-    .into_iter()
-    .collect();
-
-    Template::render("back_office", context)
+    Template::render("back_office", load_texts())
 }
--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,33 @@ extern crate rocket;
 mod back_office;
 
 use rocket_contrib::{serve::StaticFiles, templates::Template};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use std::fs;
 
-#[derive(Clone, Serialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct Texts {
-    homepage_1: String,
-    homepage_2: String,
+    actors: ActorsText,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+pub struct ActorsText {
+    fda: String,
+    ttandme: String,
+    google: String,
+    scientists: String,
+    anne_wo1: String,
+    anne_wo2: String,
+}
+
+pub fn load_texts() -> Texts {
+    let file = fs::read_to_string("static/texts.ron").expect("Couldn't load texts file");
+
+    ron::from_str(&file).expect("couldn't deserialize ron file")
 }
 
 #[get("/")]
 fn homepage() -> Template {
-    Template::render(
-        "homepage",
-        Texts {
-            homepage_1: String::from("hello"),
-            homepage_2: String::from("hello2"),
-        },
-    )
+    Template::render("homepage", "")
 }
 
 #[get("/about")]
@@ -32,7 +42,8 @@ fn about() -> Template {
 
 #[get("/actors")]
 fn actors() -> Template {
-    Template::render("actors", "")
+    let texts = load_texts();
+    Template::render("actors", texts.actors)
 }
 
 fn main() {
